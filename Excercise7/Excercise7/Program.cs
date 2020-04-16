387fd0b1f418f2f4b2ec4f8c23acad40a703336c
pstevic <patrickstevic@outlook.com>
2020-04-15 21:59:57 -0400

    Excercise 7

---
+++ Excercise7/Excercise7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Excercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userResponse;
+            do
+            {
+                Console.WriteLine("Hello World! ECHOOOOOO!");
+                Console.WriteLine("Would you like to continue y/n");
+                userResponse = Console.ReadLine();
+            } while (userResponse == "y");
+
+            Console.WriteLine("Goodbye");
+
+        }
+    }
+}
