c1a62564dadf8de8386a50d6666f5488053d32b8
pstevic <patrickstevic@outlook.com>
2020-04-15 21:43:01 -0400

    Hello World

---
+++ Exercise6/Exercise6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static string tesetResponse = ""; 
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Hello World!");
+                Console.WriteLine("Would you like to continue y/n");
+                string userResponse = Console.ReadLine();
+                tesetResponse = userResponse; 
+            } while (tesetResponse == "y");
+        }
+    }
+}
