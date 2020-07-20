ecd56d64d5437e96ed5b068227af92f84d94b1ac
pstevic <patrickstevic@outlook.com>
2020-07-20 18:27:59 -0400

    lab22

---
+++ Lab.22/Lab.22/Lab.22/Controllers/MovieControler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab._22.Models;
+
+namespace Lab._22.Controllers
+{
+    public class MovieControler : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(MovieList MovieList)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(MovieList);
+        }
+    }
+}
